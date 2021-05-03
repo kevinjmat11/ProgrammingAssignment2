408dbee2e49028cba160f1aa7926bdf7d452b0dc
kevinjmat11 <81849839+kevinjmat11@users.noreply.github.com>
2021-05-03 16:17:32 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -13,5 +13,3 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
-$ git config --global user.name"kevinjmat11"
-$ git config --global user.email"81849839+kevinjmat11@users.noreply.github.com"
